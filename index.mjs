c0e41203cbc3bcbf8b218df65a0d46e202f1ef53
arvidbt <dv20arn@cs.umu.se>
2023-02-18 19:48:31 +0100

    init

---
+++ index.mjs
@@ -0,0 +1,34 @@
+import { Configuration, OpenAIApi } from "openai";
+import bodyParser from "body-parser";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = 1337;
+
+app.use(bodyParser.json());
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+app.post("/image", async (req, res) => {
+  const { prompt } = req.body;
+  const response = await openai.createImage({
+    prompt: prompt,
+    n: 1,
+    size: "1024x1024",
+  });
+
+  res.send(response.data.data[0].url);
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
+
+// OPENAI_API_KEY="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"
